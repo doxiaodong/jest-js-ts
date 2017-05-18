4a96a0ec3f6f51a96415e48a0b1cded398427271
duxiaodong <duxiaodong@darlin.me>
2017-05-18 13:27:07 +0800

    add

---
+++ js/__tests__/local.js
@@ -0,0 +1,8 @@
+import { add } from '../local'
+
+describe('local test', () => {
+  add(1, 2)
+  test('local', () => {
+
+  })
+})
---
+++ ts/__tests__/lodash-es.ts
@@ -0,0 +1,8 @@
+import { extend } from 'lodash-es'
+
+describe('lodash-es test', () => {
+  extend({}, {})
+  test('lodash-es', () => {
+
+  })
+})
